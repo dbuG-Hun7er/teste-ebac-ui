e258acfd4bd12b367a6110bb924ac69fbac8d2e5
dbuG-Hun7er <dry_lucas@outlook.com>
2024-12-16 01:14:33 -0300

    melhoria no code

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade: Login', () => {
 //Hooks usando beforeeach para facilitar a rotaçao e aftereach para fazer screnshote etcs
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+        cy.visit('/minha-conta/')
      });
 
      afterEach(() => {
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,17 +1,11 @@
-/// <reference types="cypress" />
+/// <reference types="cypress"/>
 
 describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block >')
-            //.first()
-            //.last()
-            //.eq(3)
-            .contains('Aero Daily Fitness Tee')
-            .click()
-            cy.get('#tab-title-additional_information > a').should('contain' , 'Informação adicional')
-        
+        cy.get('.product-block').eq(1).click()
+        cy.get('.button-variable-item-Green').should('contain','Green')
     });
 });
