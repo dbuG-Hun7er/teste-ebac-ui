2bd1eabd0b9ba4f8c9ab45e8e3e76e5a181fa529
dbuG-Hun7er <dry_lucas@outlook.com>
2025-01-14 23:27:09 -0300

    foi add pasta .vscode

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,60 +1,67 @@
 /// <reference types="cypress" />
 //massa de dados
-const perfil = require('../../fixtures/perfil.json')
+const perfil = require("../../fixtures/perfil.json");
 //teste de login loja EBAC
-describe('Funcionalidade: Login', () => {
-//Hooks usando beforeeach para facilitar a rotaçao e aftereach para fazer screnshote etcs
-    beforeEach(() => {
-        cy.visit('/minha-conta/')
-     });
+describe("Funcionalidade: Login", () => {
+  //Hooks usando beforeeach para facilitar a rotaçao e aftereach para fazer screnshote etcs
+  beforeEach(() => {
+    cy.visit("/minha-conta/");
+  });
 
-     afterEach(() => {
-        cy.screenshot()
-     });
-//teste de login, cenario sucesso
-    it('Deve fazer login com sucesso', () => {
-        
-        cy.get('#username').type('lucas.teste@teste.com')
-        cy.get('#password').type('teste@123')
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lucas.teste (não é lucas.teste? Sair)')
+  //afterEach(() => {cy.screenshot()});
+  //teste de login, cenario sucesso
+  it("Deve fazer login com sucesso", () => {
+    cy.get("#username").type("lucas.teste@teste.com");
+    cy.get("#password").type("teste@123");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-MyAccount-content");
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+      "contain",
+      "Olá, lucas.teste (não é lucas.teste? Sair)"
+    );
+  });
+  //teste de login, cenario usuário inválido
+  it("deve exibir uma mensagem de erro ao inserir usuário inválido", () => {
+    cy.get("#username").type("usuario.invalido@teste.com");
+    cy.get("#password").type("teste@123");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-error").should(
+      "contain",
+      "Endereço de e-mail desconhecido."
+    );
+    cy.get(".woocommerce-error").should("exist");
+  });
+  //teste de login, cenario senha inválida
+  it("Deve exibir uma mensagem de erro ao inserir senha invalida", () => {
+    cy.get("#username").type("lucas.teste@teste.com");
+    cy.get("#password").type("senhaInvalidade");
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-error").should(
+      "contain",
+      "Erro: A senha fornecida para o e-mail lucas.teste@teste.com está incorreta. Perdeu a senha?"
+    );
+    cy.get(".woocommerce-error").should("exist");
+  });
 
-    });
-//teste de login, cenario usuário inválido
-    it('deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-       
-        cy.get('#username').type('usuario.invalido@teste.com')
-        cy.get('#password').type('teste@123')
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
-        cy.get('.woocommerce-error').should('exist')        
-    });
-//teste de login, cenario senha inválida
-    it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
-        
-        cy.get('#username').type('lucas.teste@teste.com')
-        cy.get('#password').type('senhaInvalidade')
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail lucas.teste@teste.com está incorreta. Perdeu a senha?')
-        cy.get('.woocommerce-error').should('exist')   
-     
-        
-    });
+  it.only("Deve fazer login com sucesso - Usando massa de dados", () => {
+    cy.get("#username").type(perfil.usuario);
+    cy.get("#password").type(perfil.senha);
+    cy.get(".woocommerce-form > .button").click();
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+      "contain",
+      "Olá, lucas.teste (não é lucas.teste? Sair)"
+    );
+  });
 
-    it('Deve gazer login com sucesso - Usando massa de dados', () => {
-        cy.get('#username').type(perfil.usuario)
-        cy.get('#password').type(perfil.senha)
-        cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lucas.teste (não é lucas.teste? Sair)') 
+  it("Deve fazer login com sucesso - Usando Fixture", () => {
+    cy.fixture("perfil").then((dados) => {
+      cy.get("#username").type(dados.usuario);
+      cy.get("#password").type(dados.senha, { log: false });
+      cy.get(".woocommerce-form > .button").click();
+      cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+        "contain",
+        "Olá, lucas.teste (não é lucas.teste? Sair)"
+      );
     });
-
-    it.only('Deve gazer login com sucesso - Usando Fixture', () => {
-       cy.fixture('perfil').then( dados => {
-            cy.get('#username').type(dados.usuario)
-            cy.get('#password').type(dados.senha , { log: false })
-            cy.get('.woocommerce-form > .button').click()
-            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lucas.teste (não é lucas.teste? Sair)') 
-       })
-    });
-})
+  });
+});
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress"/>
 
-describe('Funcionalidade: Produtos', () => {
-    beforeEach(() => {
-        cy.visit('produtos')
-    });
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block').eq(1).click()
-        cy.get('.button-variable-item-Green').should('contain','Green')
-    });
-})
+describe("Funcionalidade: Produtos", () => {
+  beforeEach(() => {
+    cy.visit("produtos");
+  });
+  it("Deve selecionar um produto da lista", () => {
+    cy.get(".product-block").eq(1).click();
+    cy.get(".button-variable-item-Brown").should("contain", "Brown");
+    //cy.get('.button-variable-item-Green').should('contain','Green')
+  });
+});
