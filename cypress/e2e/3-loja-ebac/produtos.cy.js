21efa4eea390fd144dd44edcb0c62c7796094b0c
dbuG-Hun7er <dry_lucas@outlook.com>
2025-01-22 01:29:40 -0300

    foi usado teste de page com massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -16,11 +16,35 @@ describe("Funcionalidade: Produtos", () => {
     produtosPage.buscarProdutoLista("Abominable Hoodie");
     cy.get("#tab-title-description > a").should("contain", "Descrição");
   });
-  it.only("Deve buscar um produto com sucesso", () => {
+  it("Deve buscar um produto com sucesso", () => {
     let produto = "Zeppelin Yoga Pant";
     produtosPage.buscarProduto(produto);
     cy.get(".product_title").should("contain", produto);
   });
-  it("Deve visitar  a página do produto", () => {});
-  it("Deve adicionar produto ao carrinho", () => {});
+  it("Deve visitar  a página do produto", () => {
+    produtosPage.visitarProduto("Zeppelin Yoga Pant");
+    cy.get(".product_title").should("contain", "Zeppelin Yoga Pant");
+  });
+  it("Deve adicionar produto ao carrinho", () => {
+    let qtde = 5;
+    produtosPage.visitarProduto("Ariel Roll Sleeve Sweatshirt");
+    produtosPage.addProdutoCarrinho("M", "Green", qtde);
+    cy.get(".woocommerce-message").should(
+      "contain",
+      qtde +
+        " × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho."
+    );
+  });
+
+  it.only("Deve adicionar produto ao carrinho buscando massa de dados", () => {
+    cy.fixture("produtos").then((dados) => {
+      produtosPage.visitarProduto(dados[1].nomeProduto);
+      produtosPage.addProdutoCarrinho(
+        dados[1].tamanho,
+        dados[1].cor,
+        dados[1].quantidade
+      );
+      cy.get(".woocommerce-message").should("contain", dados[1].nomeProduto);
+    });
+  });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -9,7 +9,16 @@ class produtosPage {
   buscarProdutoLista(nomeProduto) {
     cy.get(".products > .row").contains(nomeProduto).click();
   }
-  visitarProcuto() {}
-  addProdutoCarrinho() {}
+  visitarProduto(nomeProduto) {
+    //cy.visit(`produtos/${nomeProduto}`);
+    const url = nomeProduto.replace(/ /g, "-");
+    cy.visit(`produtos/${url}`);
+  }
+  addProdutoCarrinho(tamanho, cor, quantidade) {
+    cy.get(".button-variable-item-" + tamanho).click();
+    cy.get(".button-variable-item-" + cor).click();
+    cy.get(".input-text").clear().type(quantidade);
+    cy.get(".single_add_to_cart_button").click();
+  }
 }
 export default new produtosPage();
